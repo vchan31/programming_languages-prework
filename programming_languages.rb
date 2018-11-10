35b9281940a3f0b97af9c552df8bef90688ca440
sensible-proc-7557 <vchan313@gmail.com>
2018-11-10 17:01:45 +0000

    Done.

--- programming_languages.rb
+++ programming_languages.rb
@@ -1,3 +1,23 @@
 def reformat_languages(languages)
-  # your code here
+ new_hash = {}
+
+languages.each do |style, language|
+  language.each do |language,attribute|
+    new_hash[language] = {style:[]}.merge(attribute)
+    end
+    
+
+
+end
+
+
+
+languages.each do |style, language|
+  language.each do |language,attribute|
+    new_hash[language][:style] << style
+    
+  end
+end
+
+new_hash
 end
